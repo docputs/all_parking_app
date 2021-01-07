9b3bb6e177a832c0d5ba635fec146cf236f1f828
João Vitor <jvsoares_@hotmail.com>
2021-01-07 05:08:26 -0300

    add admin parameter to user

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -4,6 +4,12 @@ class User {
   final String id;
   final String displayName;
   final String email;
+  final bool isAdmin;
 
-  const User({@required this.id, @required this.displayName, @required this.email});
+  const User({
+    @required this.id,
+    @required this.displayName,
+    @required this.email,
+    this.isAdmin = false,
+  });
 }
