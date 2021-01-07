e84c9147826c1facbaa5c25e66446b714febba1a
João Vitor <jvsoares_@hotmail.com>
2021-01-07 05:04:52 -0300

    add user entity

---
+++ lib/features/auth/domain/entities/user.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+
+class User {
+  final String id;
+  final String displayName;
+  final String email;
+
+  const User({@required this.id, @required this.displayName, @required this.email});
+}
