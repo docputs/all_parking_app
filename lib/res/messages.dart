1f64b4ff4cc4865cd25cce6282c57f8d3abc3a31
João Vitor <jvsoares_@hotmail.com>
2021-01-14 17:51:46 -0300

    add app constant strings

---
+++ lib/res/messages.dart
@@ -0,0 +1,16 @@
+class Messages {
+  //AuthFailure
+  static const emailBadlyFormatted = 'E-mail inválido';
+  static const emptyField = 'Campo obrigatório';
+  static const unknownFailure = 'Erro desconhecido';
+  static const serverFailure = 'Erro no servidor';
+  static const invalidEmailAndPasswordCombination = 'E-mail e/ou senha inválidos';
+  static const cancelledByUser = 'Cancelado pelo usuário';
+
+  static const emailFieldLabel = 'E-mail';
+  static const passwordFieldLabel = 'Senha';
+
+  //SignIn
+  static const signInScreenTitle = 'Acesse sua conta';
+  static const signInButton = 'ENTRAR';
+}
