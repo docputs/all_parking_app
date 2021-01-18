731a5781ef208f02b088605121868f0e2e954005
João Vitor <jvsoares_@hotmail.com>
2021-01-18 20:12:29 -0300

    save manager data to repository when signed up

--- lib/features/parking/domain/usecases/add_parking_lot.dart
+++ lib/features/parking/domain/usecases/add_parking_lot.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
+import 'package:kt_dart/kt.dart';
+import '../../core/errors/manager_failure.dart';
 import '../../core/errors/parking_failure.dart';
 import '../entities/parking_lot.dart';
 import '../repositories/i_manager_repository.dart';
@@ -11,16 +13,33 @@ class AddParkingLot {
   final IParkingLotRepository _parkingLotRepository;
   final IManagerRepository _managerRepository;
 
-  const AddParkingLot(this._parkingLotRepository, this._managerRepository);
+  const AddParkingLot(this._parkingLotRepository, this._managerRepository)
+      : assert(_parkingLotRepository != null, _managerRepository != null);
 
   Future<Either<ParkingFailure, Unit>> call(ParkingLot parkingLot) async {
     final parkingLotEither = await _parkingLotRepository.create(parkingLot);
     return parkingLotEither.fold(
       (f) => left(f),
       (_) async {
-        final managerEither = await _managerRepository.addParkingLot(parkingLot);
-        return managerEither.fold(
+        final either = await caseParkingLotEitherSuccess(parkingLot);
+        return either.fold(
           (f) => left(const ParkingFailure.serverFailure()),
+          (_) => right(unit),
+        );
+      },
+    );
+  }
+
+  Future<Either<ManagerFailure, Unit>> caseParkingLotEitherSuccess(ParkingLot parkingLot) async {
+    final managerEither = await _managerRepository.read();
+    return managerEither.fold(
+      (f) => left(f),
+      (manager) async {
+        final newList = manager.parkingLots.plusElement(parkingLot.id);
+        final newManager = manager.copyWith(parkingLots: newList);
+        final updateEither = await _managerRepository.update(newManager);
+        return updateEither.fold(
+          (f) => left(f),
           (_) => right(unit),
         );
       },
