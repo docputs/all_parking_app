defe46745746aed5053f463e582ec7ae7d88e064
João Vitor <jvsoares_@hotmail.com>
2021-02-01 13:23:44 -0300

    add check-in and check-out FAB on home screen

--- lib/features/parking/presentation/home/screens/home_screen.dart
+++ lib/features/parking/presentation/home/screens/home_screen.dart
@@ -4,11 +4,12 @@ import 'package:all_parking/features/parking/presentation/current_parking_lot.da
 import 'package:all_parking/features/parking/presentation/home/screens/components/custom_app_bar.dart';
 import 'package:all_parking/features/parking/presentation/home/screens/components/parking_lot_dashboard.dart';
 import 'package:all_parking/res/constants.dart';
+import 'package:all_parking/res/theme.dart';
 import 'package:all_parking/widgets/no_parking_lots_found.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
+import 'package:hawk_fab_menu/hawk_fab_menu.dart';
 
 import '../../../../../service_locator.dart';
 import '../../../../../widgets/app_scaffold.dart';
@@ -34,8 +35,24 @@ class HomeScreen extends StatelessWidget {
         child: AppScaffold(
           customAppBar: CustomAppBar(),
           drawer: const DefaultDrawer(),
-          body: _buildBody(),
-          floatingActionButton: _buildCustomFAB(),
+          body: HawkFabMenu(
+            fabColor: AppColors.accentColor,
+            body: _buildBody(),
+            items: [
+              HawkFabMenuItem(
+                label: 'CHECK-IN',
+                labelBackgroundColor: Colors.white,
+                icon: Icon(Icons.south_east),
+                ontap: () {},
+              ),
+              HawkFabMenuItem(
+                label: 'CHECK-OUT',
+                labelBackgroundColor: Colors.white,
+                icon: Icon(Icons.north_east),
+                ontap: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -50,13 +67,6 @@ class HomeScreen extends StatelessWidget {
           (parkingLot) => ParkingLotDashboard(parkingLot),
         );
       },
-    );
-  }
-
-  Widget _buildCustomFAB() {
-    return FloatingActionButton(
-      onPressed: () {},
-      child: Icon(Icons.directions_car),
     );
   }
 }
