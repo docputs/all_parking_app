dc2ddf1a66fc7ae7d73907d2da2778f382e7fe40
João Vitor <docputs@gmail.com>
2021-03-04 13:48:10 -0300

    comment edit profile drawer item

--- lib/res/drawer_messages.dart
+++ lib/res/drawer_messages.dart
@@ -11,13 +11,13 @@ class DrawerMessages {
     const DrawerItem('Gerenciar estacionamentos', icon: Icons.local_parking, route: Constants.manageParkingLotsRoute),
     const DrawerItem('Relatórios', icon: Icons.history, route: Constants.reportsRoute),
     const DrawerItem('Veículos estacionados', icon: Icons.directions_car, route: Constants.parkedVehiclesRoute),
-    const DrawerItem('Editar perfil', icon: Icons.edit, route: Constants.editInformationRoute),
+    // const DrawerItem('Editar perfil', icon: Icons.edit, route: Constants.editInformationRoute),
   ];
 
   static final _employeeItems = [
     const DrawerItem('Início', icon: Icons.home, route: Constants.homeRoute),
     const DrawerItem('Veículos estacionados', icon: Icons.directions_car, route: Constants.parkedVehiclesRoute),
-    const DrawerItem('Editar perfil', icon: Icons.edit, route: Constants.editInformationRoute),
+    // const DrawerItem('Editar perfil', icon: Icons.edit, route: Constants.editInformationRoute),
   ];
 
   static final items = AppConfig.isManager ? _managerItems : _employeeItems;
